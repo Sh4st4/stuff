2a5b14e13d684e87ba059c78e2fbca3091b4161d
ppp <34691058+Sh4st4@users.noreply.github.com>
2017-12-27 02:13:58 +0700

    It is what it is
    Meow

---
+++ List overlap awful_solution.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+  
+if 1 in a and b:
+    c.extend([1])
+if 2 in a and b:
+    c.extend([2])
+if 3 in a and b:
+    c.extend([3])
+if 4 in a and b:
+    c.extend([4])
+if 5 in a and b:
+    c.extend([5])
+if 6 in a and b:
+    c.extend([6])
+if 7 in a and b:
+    c.extend([7])
+if 8 in a and b:
+    c.extend([8])
+if 9 in a and b:
+    c.extend([9])
+if 10 in a and b:
+    c.extend([10])
+if 11 in a and b:
+    c.extend([11])
+if 12 in a and b:
+    c.extend([12])
+if 13 in a and b:
+    c.extend([13])
+    print(c)
+    
